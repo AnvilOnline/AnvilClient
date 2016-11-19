670c9a718f382f11f351c3346e51f4a4d060d0d0
Camden Smallwood <camden.smallwood@gmail.com>
2016-11-18 19:40:43 -0500

    Removed unimplemented audio hooks in AnvilEldorado

--- AnvilEldorado/Source/Patches/Audio.cpp
+++ AnvilEldorado/Source/Patches/Audio.cpp
@@ -4,44 +4,11 @@
 using namespace AnvilCommon;
 using namespace AnvilEldorado::Patches;
 
-__declspec(naked) void FmodSystemInitHook()
-{
-	__asm
-	{
-		push	0; extradriverdata
-		push	ebx; flags
-		push	100h; maxchannels
-		push	eax; FMOD_SYSTEM
-		call	dword ptr[ecx]; FMOD::System::init
-		push	0x404EA4
-		ret
-	}
-}
-
-__declspec(naked) void FmodSystemInitHook2()
-{
-	__asm
-	{
-		push	0; extradriverdata
-		push	ebx; flags
-		push	100h; maxchannels
-		push	eax; FMOD_SYSTEM
-		call	dword ptr[ecx]; FMOD::System::init
-		push	0x404EC8
-		ret
-	}
-}
-
 void AnvilPatch::Patch_Audio()
 {
 	// Adds the FMOD WASAPI output fix from FMODEx 4.44.56, which stops weird popping sound at startup
 	// TODO: maybe find a way to update HO's FMOD, HO is using 4.26.6 which is ancient
 	Utils::Util::PatchAddressInFile(0x100DA75, "\x02", 1);
-
-	// increase max virtual audio channels from 64 to 256
-	// http://www.fmod.org/docs/content/generated/FMOD_System_Init.html
-	Utils::Util::ApplyHook(0x4E9C, FmodSystemInitHook);
-	Utils::Util::ApplyHook(0x4EC0, FmodSystemInitHook2);
 
 	// increase software channels from 192 to 256
 	// http://www.fmod.org/docs/content/generated/FMOD_System_SetSoftwareChannels.html
