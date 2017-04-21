55ed0cb7cef917bd5291c4792d4b78740a4a9978
medsouz <medsouz99@gmail.com>
2017-04-20 20:53:25 -0400

    Fix start_new_campaign stringid
    Use the integer ID of strings instead of the value

--- AnvilEldorado/Source/Game/UI/Hooks.cpp
+++ AnvilEldorado/Source/Game/UI/Hooks.cpp
@@ -4,8 +4,6 @@
 
 #include <BuildInfo.hpp>
 
-#include <string>
-
 #include <Game\UI\UIImpl.hpp>
 
 using namespace AnvilEldorado::Game::UI;
@@ -27,7 +25,7 @@ DeclareDetouredFunction(Hooks, bool, __cdecl, LocalizeString, int p_TagIndex, Bl
 {
 	const static size_t MaxStringLength = 0x400;
 
-	if (p_StringId == "start_new_campaign")
+	if (p_StringId == 0x0001010A /* start_new_campaign */)
 	{
 #if _DEBUG
 		swprintf_s(p_Output, MaxStringLength, L"Anvil: Online - Dev Build - %d", ANVIL_BUILD);
